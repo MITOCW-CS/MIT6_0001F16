3ad7f943e3eb8678651fb9094ebccff45769161a
mitocw-cs <mitocw.cs@gmail.com>
2017-08-12 09:27:14 +0700

    MIT6_0001F16

---
+++ Lectures/lec8/AnimalManager.py
@@ -0,0 +1,52 @@
+"""
+Animal manager
+
+                  Animal
+              ______|______
+             |      |      |
+           Rabbit  Cat   Person
+                           |
+                        Student
+
+1. Animal
+    - Attributes:
+        + age
+        + name
+    - Methods:
+        + init(age)
+        + get_age()
+        + get_name()
+        + set_age(new_age)
+        + set_name(new_name)
+        + str()
+2. Cat
+    - Attributes:
+        None
+    - Methods:
+        + speak()
+        + str()
+3. Person
+    - Attributes:
+        + name
+        + age
+        + friends[]
+    - Methods:
+        + get_friends()
+        + speak()
+        + add_friends()
+        + age_diff()
+        + str()
+4. Student
+    - Attributes:
+        + name
+        + age
+        + major
+    - Methods:
+        + str()
+        + change_major()
+        + speak()
+5. Rabbit
+    - Attributes:
+        
+"""
+
